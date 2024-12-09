a68627809652922537c33c97c8b35f1c207062e1
Prithak koirala <prithakkoirala44@gmail.com>
2024-12-09 20:44:12 +0545

    The converter works but gotta style and debug

--- project6/currency.js
+++ project6/currency.js
@@ -1,1 +1,60 @@
-console.log("one quater done");
+function submitted(){
+    const c1 = document.getElementById("selection1").value;
+    const c2 = document.getElementById("selection2").value;
+    let final;
+    let finalin=document.getElementById("amount").value;
+   switch (c1){
+    case "NRS(Re)" :
+        switch (c2){
+            case "NRS(Re)":
+            final=document.getElementById("amount").value;
+            break;
+
+            case "USD($)":
+            final=(document.getElementById("amount").value)/132.50;
+            break;
+
+            case "Euros(£)":
+            final=(document.getElementById("amount").value)/144.56;
+            break;
+    
+        }
+    break;
+
+    case "USD($)" :
+        switch (c2){
+            case "NRS(Re)":
+            final=(document.getElementById("amount").value)*132.50;
+            break;
+
+            case "USD($)":
+            final=document.getElementById("amount").value;
+            break;
+
+            case "Euros(£)":
+            final=(document.getElementById("amount").value)/0.92;
+            break;
+    
+        }
+    break;
+
+    case "Euros(£)" :
+        switch (c2){
+            case "NRS(Re)":
+            final=(document.getElementById("amount").value)*144.56;
+            break;
+
+            case "USD($)":
+            final=(document.getElementById("amount").value)*0.92;
+            break;
+
+            case "Euros(£)":
+            final=document.getElementById("amount").value;
+            break;
+    
+        }
+    break;
+   }
+   final = final.toFixed(2);
+   document.getElementById("outputDiv").innerText=`${c1} ${finalin} = ${c2} ${final}`
+}
